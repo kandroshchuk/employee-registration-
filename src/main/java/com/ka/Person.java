894f2da104f8189af79586efb06d5b13a7c8adad
kandroshchuk <kandroshchuk@gmail.com>
2016-08-18 21:18:08 -0700

    Create Person Class

---
+++ src/main/java/com/ka/Person.java
@@ -0,0 +1,22 @@
+/**
+ * File Name: Person.java<br>
+ * Androshchuk, Kateryna<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Aug 18, 2016
+ */
+package com.ka;
+
+/**
+ * Person //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Androshchuk, Kateryna
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class Person {
+}
